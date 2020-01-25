a8d511afc1fa295b1fb41af07a1eac8c08592bd5
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-25 14:12:21 -0800

    completed exercises, but unsure of #3

--- exercise.sql
+++ exercise.sql
@@ -4,10 +4,7 @@ SELECT
     d.dept_name as Department
 FROM
     employee e
-    JOIN
-    department d 
-    ON
-    e.department = d.id
+    JOIN department d ON e.department = d.id
 WHERE
     d.dept_name = 'Sales';
 
@@ -18,12 +15,8 @@ SELECT
     p.project_name as Project 
 FROM
     employee e
-    JOIN
-    employee_project ep 
-    ON e.id = ep.emp_id
-    JOIN
-    project p
-    ON ep.project_id = p.id
+    JOIN employee_project ep ON e.id = ep.emp_id
+    JOIN project p ON ep.project_id = p.id
 WHERE p.id = 2;
 
 -- 3. List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
@@ -33,18 +26,33 @@ SELECT
     p.project_name as Project
 FROM
     employee e 
-    JOIN
-    department d 
-    ON e.department = d.id
-    JOIN
-    employee_project ep 
-    ON e.id = ep.emp_id
-    JOIN
-    project p 
-    ON ep.project_id = p.id 
+    JOIN department d ON e.department = d.id
+    JOIN employee_project ep  ON ep.emp_id = e.id
+    JOIN project p ON ep.project_id = p.id 
 WHERE
-    d.id = 4
-    AND p.id = 4;
+    p.project_name = 'Watch paint dry' AND d.dept_name = 'Warehouse';
 
 -- 4. Which projects are the Sales department employees assigned to?
+SELECT
+    e.emp_name as Full_Name,
+    p.project_name as Project,
+    d.dept_name as Department
+FROM
+    employee e
+    JOIN department d ON e.department = d.id
+    JOIN employee_project ep ON ep.emp_id = e.id
+    JOIN project p ON ep.project_id = p.id
+WHERE
+    d.dept_name = 'Sales';
 -- 5. List only the managers that are assigned to the 'Watch paint dry' project.
+SELECT
+    e.emp_name as Full_Name,
+    p.project_name as Project,
+    d.manager as Manager
+FROM
+    employee e 
+    JOIN employee_project ep ON ep.emp_id = e.id
+    JOIN project p ON ep.project_id = p.id
+    JOIN department d ON e.department = d.id
+WHERE
+    p.project_name = 'Watch paint dry' AND d.manager = ep.emp_id;
